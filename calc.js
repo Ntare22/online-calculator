b9aae0ac2e6568271f09dd36549c32307405db1a
Ntare22 <jim.musika2016@kepler.org>
2018-04-23 22:00:47 +0200

    first phase, just finished with initial phases

---
+++ calc.js
@@ -0,0 +1,22 @@
+var screen = document.getElementById("screen")
+var number1 = document.getElementById("num1").innerHTML;
+var number2 = document.getElementById("num2").innerHTML;
+var number3 = document.getElementById("num3").innerHTML;
+var number4 = document.getElementById("num4").innerHTML;
+var number5 = document.getElementById("num5").innerHTML;
+var number6 = document.getElementById("num6").innerHTML;
+var number7 = document.getElementById("num7").innerHTML;
+var number8 = document.getElementById("num8").innerHTML;
+var number9 = document.getElementById("num9").innerHTML;
+var number0 = document.getElementById("num0").innerHTML;
+var plussign = document.getElementById("plus").innerHTML;
+var multiplication = document.getElementById("multiply").innerHTML;
+var division = document.getElementById("divide").innerHTML;
+var subtraction = document.getElementById("minus").innerHTML;
+var point = document.getElementById("point").innerHTML;
+var equate = document.getElementById("equals").innerHTML;
+
+function number() { alert("clicked 1")};
+
+number1.onlick = number()
+
