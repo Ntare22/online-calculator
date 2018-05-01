0eb56ef20c031b8456dc240fd5dd5a41d59710b1
Jim Musika Ntare <jimnm2018@gmail.com>
2018-05-01 19:33:16 +0200

    some improvements, you can now enter number into the screen

--- calc.js
+++ calc.js
@@ -1,35 +1,22 @@
-var screen = document.getElementById("screen")
-var number1 = document.getElementById("num1");
-var number2 = document.getElementById("num2");
-var number3 = document.getElementById("num3");
-var number4 = document.getElementById("num4");
-var number5 = document.getElementById("num5");
-var number6 = document.getElementById("num6");
-var number7 = document.getElementById("num7");
-var number8 = document.getElementById("num8");
-var number9 = document.getElementById("num9");
-const number0 = document.getElementById("num0");
-var plussign = document.getElementById("plus");
-var multiplication = document.getElementById("multiply");
-var division = document.getElementById("divide");
-var subtraction = document.getElementById("minus");
-var point = document.getElementById("point");
-var equate = document.getElementById("equals");
 
 
+let screen = document.getElementById("screen")
+var allButtons = document.getElementsByClassName('buttons');
 
+	
 
-function enterNumber(numbers) {
-	var numbers = [number0, number1,number2,number3, number4, number5, number6, number7, number8, number9];
-		for (var i = 0; i < numbers.length; i++) {
-			
-			// function to help us click on numbers
-			function clickNumbers(event) {
-				alert("You clicked a number");
+for (var i = 0; i < allButtons.length; i++) {
+	allButtons[i].addEventListener('click', pressButton)
+}
 
-			};
-
-		numbers[i].addEventListener('click', clickNumbers());
-	}
+function pressButton(event){
+	let numID = this.id; // activates ID for each button the event listener above
+	let numValue = this.value;
+	screen.innerHTML = numValue;
+	concantenateNum()
 };
+
+function concantenateNum() {
+	
+}
 
